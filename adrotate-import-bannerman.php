bcc0624827a87352a9d36040339c0a2afa24767b
adegans <adegans@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-06 12:54:06 +0000

    Version 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/adrotate-switch/tags/1.1.2@1083801 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adrotate-import-bannerman.php
@@ -0,0 +1,112 @@
+<?php
+/* ------------------------------------------------------------------------------------
+*  COPYRIGHT AND TRADEMARK NOTICE
+*  Copyright 2008-2014 AJdG Solutions (Arnan de Gans). All Rights Reserved.
+*  ADROTATE is a trademark (pending registration) of Arnan de Gans.
+
+*  COPYRIGHT NOTICES AND ALL THE COMMENTS SHOULD REMAIN INTACT.
+*  By using this code you agree to indemnify Arnan de Gans from any
+*  liability that might arise from it's use.
+------------------------------------------------------------------------------------ */
+
+function adrotateswitch_import_bannerman() {
+	global $wpdb, $current_user, $userdata, $adrotate_config;
+	
+	if(!adrotateswitch_adrotate_is_active()) {
+		wp_redirect('admin.php?page=adrotate-switch');
+		die();
+	} 
+
+	$now = adrotate_now();
+
+	if(wp_verify_nonce($_POST['adrotateswitch_nonce'], 'adrotateswitch_import_bannerman')) {
+		$data = maybe_unserialize(get_option('bannerman'));
+
+		// Convert interfal
+		if($data['refresh'] == 30) $data['refresh'] = 35;
+		if($data['refresh'] == 40) $data['refresh'] = 45;
+		if($data['refresh'] == 50) $data['refresh'] = 45;
+
+		// Determine loation
+		if($data['display'] == 'none') $data['display'] = 0;
+		if($data['display'] == 'top') $data['display'] = 1;
+		if($data['display'] == 'bottom') $data['display'] = 2;
+
+		// List all pages
+		$pages = get_pages(array('sort_column' => 'ID', 'sort_order' => 'asc'));
+		$page_list = '';	
+		if(!empty($pages)) {
+			foreach($pages as $page) {
+				$page_list .= $page_list.','.$page->ID;
+			}
+		}
+
+		$groupdata['name'] = '[Imported] BannerMan';
+		$groupdata['modus'] = ($data['refresh'] > 0) ? 1 : 0;
+		$groupdata['fallback'] = 0;
+		$groupdata['sortorder'] = 0;
+		$groupdata['cat'] = '';
+		$groupdata['cat_loc'] = '';
+		$groupdata['cat_par'] = '';
+		$groupdata['page'] = $page_list;
+		$groupdata['page_loc'] = $data['display'];
+		$groupdata['page_par'] = '';
+		$groupdata['geo'] = 0;
+		$groupdata['wrapper_before'] = '<center>';
+		$groupdata['wrapper_after'] = '</center>';
+		$groupdata['gridrows'] = 2;
+		$groupdata['gridcolumns'] = 2;
+		$groupdata['admargin'] = 0;
+		$groupdata['admargin_bottom'] = 0;
+		$groupdata['admargin_left'] = 0;
+		$groupdata['admargin_right'] = 0;
+		$groupdata['adwidth'] = 125;
+		$groupdata['adheight'] = 125;
+		$groupdata['adspeed'] = ($data['refresh'] > 0) ? $data['refresh']*1000 : 6000;
+
+		$wpdb->insert($wpdb->prefix."adrotate_groups", $groupdata);
+		$group_id = $wpdb->insert_id;
+
+		foreach($data['banners'] as $key => $value) {
+			$new_id = $key + 1;
+			// Format advert
+			$advertdata['title'] = '[Imported] BannerMan banner '.$new_id;
+			$advertdata['bannercode'] = esc_attr($value);
+			$advertdata['thetime'] = $now;
+			$advertdata['updated'] = $now;
+			$advertdata['author'] = $current_user->user_login;
+			$advertdata['imagetype'] = '';
+			$advertdata['image'] = '';
+			$advertdata['link'] = '';
+			$advertdata['tracker'] = 'N';
+			$advertdata['responsive'] = 'N';
+			$advertdata['type'] = 'active';
+			$advertdata['weight'] = 6;
+			$advertdata['sortorder'] = 0;
+			$advertdata['cbudget'] = 0;
+			$advertdata['ibudget'] = 0;
+			$advertdata['crate'] = 0;
+			$advertdata['irate'] = 0;
+			$advertdata['cities'] = array();
+			$advertdata['countries'] = array();
+	
+			$wpdb->insert($wpdb->prefix."adrotate", $advertdata);
+		    $ad_id = $wpdb->insert_id;
+
+			$end_date = $now + 2592000;
+
+			$wpdb->insert($wpdb->prefix.'adrotate_schedule', array('name' => 'Imported schedule for advert '.$ad_id, 'starttime' => $now, 'stoptime' => $end_date, 'maxclicks' => 0, 'maximpressions' => 0, 'spread' => 'N', 'dayimpressions' => 0));
+			$schedule_id = $wpdb->insert_id;
+			$wpdb->insert($wpdb->prefix.'adrotate_linkmeta', array('ad' => $ad_id, 'group' => 0, 'block' => 0, 'user' => 0, 'schedule' => $schedule_id));
+
+			$wpdb->insert($wpdb->prefix.'adrotate_linkmeta', array('ad' => $ad_id, 'group' => $group_id, 'block' => 0, 'user' => 0, 'schedule' => 0));
+			
+			unset($advertdata, $ad_id, $groupdata, $group_id, $schedule_id);
+		}
+	
+		wp_redirect('admin.php?page=adrotate-switch&s=1');
+	} else {
+		adrotate_nonce_error();
+	}
+}
+?>
---
+++ adrotate-import-ubm.php
@@ -0,0 +1,94 @@
+<?php
+/* ------------------------------------------------------------------------------------
+*  COPYRIGHT AND TRADEMARK NOTICE
+*  Copyright 2008-2014 AJdG Solutions (Arnan de Gans). All Rights Reserved.
+*  ADROTATE is a trademark (pending registration) of Arnan de Gans.
+
+*  COPYRIGHT NOTICES AND ALL THE COMMENTS SHOULD REMAIN INTACT.
+*  By using this code you agree to indemnify Arnan de Gans from any
+*  liability that might arise from it's use.
+------------------------------------------------------------------------------------ */
+
+function adrotateswitch_import_ubm() {
+	global $wpdb, $current_user, $userdata, $adrotate_config;
+	
+	if(!adrotateswitch_adrotate_is_active()) {
+		wp_redirect('admin.php?page=adrotate-switch');
+		die();
+	} 
+
+	$now = adrotate_now();
+
+	if(wp_verify_nonce($_POST['adrotateswitch_nonce'], 'adrotateswitch_import_ubm')) {
+		$include_schedules = (isset($_POST['adrotateswitch_import_ubm_schedules'])) ? 1 : 0;
+		
+		$adverts = $wpdb->get_results("SELECT * FROM `".$wpdb->prefix."useful_banner_manager_banners` ORDER BY `id` ASC;");
+		foreach($adverts as $advert) {
+			// target attr
+			$new_window = ' target="'.esc_attr($advert->link_target).'"';
+				
+			// rel attr
+			$link_rel = ' rel="'.esc_attr($advert->link_rel).'"';
+				
+			// alt attr
+			$alt_attr = ' alt="'.esc_attr($advert->banner_alt).'"';
+				
+			// Use jQuery Clicktracker?
+			if($adrotate_config['clicktracking'] == 'Y') {
+				$target_url = esc_attr($advert->banner_link);
+				$track_link = '';
+			} else {
+				$target_url = '%link%';
+				$track_link = esc_attr($advert->banner_link);
+			}				
+			$adcode = '<a href="'.$target_url.'"'.$new_window.' '.$link_rel.'><img'.$alt_attr.' src="%image%" /></a>';
+
+			// Enabled or Disabled
+			if($advert->is_visible == 'yes') {
+				$status = 'active';
+			} else {
+				$status = 'disabled';
+			}
+
+			// Format advert
+			$advertdata['title'] = '[Imported] '.esc_attr($advert->banner_title);
+			$advertdata['bannercode'] = $adcode;
+			$advertdata['thetime'] = $now;
+			$advertdata['updated'] = $now;
+			$advertdata['author'] = $current_user->user_login;
+			$advertdata['imagetype'] = 'field';
+			$advertdata['image'] = esc_attr($advert->id).'-'.esc_attr($advert->banner_name).'.'.esc_attr($advert->banner_type);
+			$advertdata['link'] = $track_link;
+			$advertdata['tracker'] = 'Y';
+			$advertdata['responsive'] = 'N';
+			$advertdata['type'] = $status;
+			$advertdata['weight'] = 6;
+			$advertdata['sortorder'] = 0;
+			$advertdata['cbudget'] = 0;
+			$advertdata['ibudget'] = 0;
+			$advertdata['crate'] = 0;
+			$advertdata['irate'] = 0;
+			$advertdata['cities'] = array();
+			$advertdata['countries'] = array();
+	
+			$wpdb->insert($wpdb->prefix."adrotate", $advertdata);
+		    $ad_id = $wpdb->insert_id;
+	
+			if($include_schedules == 1) {
+				list($eyear, $emonth, $eday) = explode('-', esc_attr($advert->active_until));
+				$end_date = gmmktime(0, 0, 0, $emonth, $eday, $eyear);
+
+				$wpdb->insert($wpdb->prefix.'adrotate_schedule', array('name' => 'Imported schedule for advert '.$ad_id, 'starttime' => $now, 'stoptime' => $end_date, 'maxclicks' => 0, 'maximpressions' => 0, 'spread' => 'N', 'dayimpressions' => 0));
+				$schedule_id = $wpdb->insert_id;
+				$wpdb->insert($wpdb->prefix.'adrotate_linkmeta', array('ad' => $ad_id, 'group' => 0, 'block' => 0, 'user' => 0, 'schedule' => $schedule_id));
+			}
+
+			unset($advertdata, $adcode, $new_window, $link_rel, $track_link, $target_url, $status, $ad_id, $schedule_id);
+		}
+	
+		wp_redirect('admin.php?page=adrotate-switch&s=1');
+	} else {
+		adrotate_nonce_error();
+	}
+}
+?>
